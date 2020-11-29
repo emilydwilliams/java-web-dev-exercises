4f0bbdfdf0c0ecfd24259281eb5db97db9b4f352
Emily Williams <emilydkoester@gmail.com>
2020-11-29 16:21:38 -0600

    completed class 3 exercises

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Course {
+
+    private String subject;
+    private ArrayList<Student> students = new ArrayList<>();
+    private HashMap<Integer, String> chapters = new HashMap<>();
+}
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,47 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String aName, int aStudentId, int aNumberOfCredits, double aGpa) {
+        this.name = aName;
+        this.studentId = aStudentId;
+        this.numberOfCredits = aNumberOfCredits;
+        this.gpa = aGpa;
+    }
+
+    public Student(String aName, int aStudentId) {
+        this(aName, aStudentId, 0, 0.0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int aStudentId) {
+        studentId = aStudentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public void setNumberOfCredits(int aNumberOfCredits) {
+        numberOfCredits = aNumberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double aGpa) {
+        gpa = aGpa;
+    }
+
 }
